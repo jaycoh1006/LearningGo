910153e2aabb8b97c49199a262d4807825e61b65
Josh <jdcohen1006@gmail.com>
2024-06-10 13:34:42 -0700

    hello world tutorial

---
+++ hello_world.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+//Run main with the commmand 'go run hello_world.go'
+//You can just compile into an executable instead with 'go build hello_world.go'
+
+func main() {
+	fmt.Println("Hello, World")
+}
